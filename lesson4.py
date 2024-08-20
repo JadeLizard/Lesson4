a35cb8997187d92b571fe135bdd9e0a3a6782c27
JadeLizard <lubovauerbah@gmail.com>
2024-08-20 15:50:44 +0300

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+number_of_hours = 1.5
+name_course = 'Python'
+number_of_hours_for_1_task = (number_of_hours/number_of_homework)
+print ('Курс',name_course,', всего задач:',number_of_homework,', затрачено часов ',number_of_hours,', среднее время выполнения ',number_of_hours_for_1_task, '.')
